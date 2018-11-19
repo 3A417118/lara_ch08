3e586d37cba16d801bbca58a8ff655add9e57903
3A417118 <s3A417118@student.ncut.edu.tw>
2018-11-19 14:31:22 +0800

    Route Subject Score

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,6 @@ Route::pattern('student_no','s[0-9]{10}');
 Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject) {
     return "學號 ".$student_no."的".$subject."成績";
 });
+Route::get('student/{student}/score/{subject?}',function($student_no,$subject = null){
+    return "學號 ".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+});
