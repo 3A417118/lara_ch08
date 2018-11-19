47fc6201c5916b315159a0db391952c7f7ac2f4f
3A417118 <s3A417118@student.ncut.edu.tw>
2018-11-19 15:13:02 +0800

    DB schema & Migration

---
+++ database/migrations/2018_11_16_101427_create_student_table.php
@@ -0,0 +1,31 @@
+<?php
+ use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+ class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('no')->uniqid();
+            $table->string('tel');
+            $table->timestamps();
+        });
+    }
+     /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
