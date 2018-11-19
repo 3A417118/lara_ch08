7d5814758ead9545c1fd3cf0e90303b6c8b0448f
3A417118 <s3A417118@student.ncut.edu.tw>
2018-11-19 15:16:15 +0800

    scopeOrderByTotal & scopeOrderBySubject

--- app/Score.php
+++ app/Score.php
@@ -1,14 +1,24 @@
-
 <?php
+ 
  namespace App;
+ 
  use Illuminate\Database\Eloquent\Model;
-use \App\Student;
+ use \App\Student;
+ 
  class Score extends Model
-{
-    //
-    protected $table = 'scores';
-    
+ {
+     //
+     protected $table = 'scores';
+     
     public function student(){
        return $this->belongsTo(Student::class);
+    }
+     public function scopeOrderByTotal($query){
+        return $query->orderBy('score.total','DESC');
+    }
+     public function scopeOrderBySubject($query){
+        return $query->orderBy('score.chinese','DESC')
+                    ->orderBy('score.english','DESC')
+                    ->orderBy('score.math','DESC');
     }
 }
