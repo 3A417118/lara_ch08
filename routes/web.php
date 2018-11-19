328ef89335286b59950bdcc51faebb56e37c055b
3A417118 <s3A417118@student.ncut.edu.tw>
2018-11-19 14:35:38 +0800

    Unitied Format

--- routes/web.php
+++ routes/web.php
@@ -31,15 +31,21 @@ Route::pattern('student_no','s[0-9]{10}');
     ])->where(['subject'=>'(chinese|english|math)']);
 });
 */
+
+Route::pattern('student_no','s3[A-Z][0-9]{6}');
+Route::pattern('subject','(chinese|english|math)');
+
  Route::get('student/{student_no}', function ($student_no) {
     return "學號：".$student_no;
-})->while({'student_no'=>'s[0-9]{10}'});
- Route::get('student/{student_no}/score', function ($student_no) {
-    return "學號 ".$student_no." 的所有成績";
-}); 
-Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject) {
-    return "學號 ".$student_no."的".$subject."成績";
 });
 Route::get('student/{student}/score/{subject?}',function($student_no,$subject = null){
     return "學號 ".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 });
+
+//Route::get('student/{student_no}/score', function ($student_no) {
+//    return "學號 ".$student_no." 的所有成績";
+//});
+ //Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject) {
+//    return "學號 ".$student_no."的".$subject."成績";
+//})->where(['student_no'=>'s3[A-Z]{6}','subject'=>'(chinese|english|math)']);
+
